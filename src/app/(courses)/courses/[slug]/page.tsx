47cd950e3d6f506db6b775cf1e3b8384b6b43bbe
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2025-07-13 18:47:46 +0330

    refactor.:courseDetail page

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,44 +1,17 @@
-// import { CourseDetails } from "@/app/_components/types/course-detail.interface";
-// import { API_URL } from "@/configs/global";
-
-// export async function generateStaticParams() {
-//   const slugs = await fetch(`${API_URL}/courses/slugs}`).then((res) =>
-//     res.json()
-//   );
-//   return (slugs as string[]).map((slug) => ({
-//     slug: slug,
-//   }));
-// }
-
-// export async function getCourse(slug: string): Promise<CourseDetails> {
-//   const res = await fetch(`${API_URL}/courses/${slug}`);
-//   return res.json();
-// }
-
-// export default async function CourseDetail({
-//   params,
-// }: {
-//   params: { slug: string };
-// }) {
-//   const { slug } = params;
-//   const courseData = await getCourse(slug);
-//   console.log("lllllllllll", courseData);
-//   return <div>{courseData.title}</div>;
-// }
-
-import { CourseDetails } from "@/app/_components/types/course-detail.interface";
+import type { CourseDetails } from "@/app/_components/types/course-detail.interface";
 import { API_URL } from "@/configs/global";
 
 export async function generateStaticParams() {
   const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
     res.json()
   );
-
+  console.log("slugs", slugs);
   return (slugs as string[]).map((slug) => ({
     slug: slug,
   }));
 }
-export async function getCourse(slug: string): Promise<CourseDetails> {
+
+async function getCourse(slug: string): Promise<CourseDetails> {
   const res = await fetch(`${API_URL}/courses/${slug}`);
   return res.json();
 }
@@ -48,10 +21,24 @@ export default async function CourseDetails({
   params: { slug: string };
 }) {
   const { slug } = params;
-  const courseData = await getCourse(slug);
+  const course = await getCourse(slug);
+
   return (
-    <div className="text-5xl flex justify-center items-center w-full">
-      <h1>{courseData.title}</h1>
+    <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
+      <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
+      <div className="col-span-10 xl:col-span-7">
+        <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+          {course.title}
+        </h1>
+        <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
+          {course.subTitle}
+        </h2>
+
+        <div className=" mt-5">Video Player Component</div>
+      </div>
+      <div className="col-span-10 xl:col-span-3 bg-secondary"></div>
+      <div className="col-span-10 xl:col-span-6 bg-info"></div>
+      <div className="col-span-10 xl:col-span-4 bg-warning"></div>
     </div>
   );
 }
